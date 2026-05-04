033d37378a8b37776a3cad44b9cc61cd4d02469b
Varun Shashwat <168959616+VarunShashwat@users.noreply.github.com>
2026-05-04 22:14:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-interview-1ddt.onrender.com",
+        //"http://localhost:5173",
     credentials:true
 }))
 
